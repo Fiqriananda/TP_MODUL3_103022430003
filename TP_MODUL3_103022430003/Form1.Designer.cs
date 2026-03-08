423de7881792cc95e600e3f4ff402a0a56adf799
Fiqriananda <fiqrianandahakin@gmail.com>
2026-03-09 02:13:43 +0800

    feat:Add Calculator Functionality

--- TP_MODUL3_103022430003/Form1.Designer.cs
+++ TP_MODUL3_103022430003/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TP_MODUL3_103022430003
+﻿    namespace TP_MODUL3_103022430003
 {
     partial class Form1
     {
@@ -51,6 +51,7 @@
             button1.TabIndex = 0;
             button1.Text = "1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += NumberButton_Click;
             // 
             // button2
             // 
@@ -60,6 +61,7 @@
             button2.TabIndex = 1;
             button2.Text = "2";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += NumberButton_Click;
             // 
             // button3
             // 
@@ -69,6 +71,7 @@
             button3.TabIndex = 2;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += NumberButton_Click;
             // 
             // button4
             // 
@@ -78,6 +81,7 @@
             button4.TabIndex = 5;
             button4.Text = "6";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += NumberButton_Click;
             // 
             // button5
             // 
@@ -87,6 +91,7 @@
             button5.TabIndex = 4;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += NumberButton_Click;
             // 
             // button6
             // 
@@ -96,7 +101,7 @@
             button6.TabIndex = 3;
             button6.Text = "4";
             button6.UseVisualStyleBackColor = true;
-            button6.Click += button6_Click;
+            button6.Click += NumberButton_Click;
             // 
             // button7
             // 
@@ -106,6 +111,7 @@
             button7.TabIndex = 8;
             button7.Text = "9";
             button7.UseVisualStyleBackColor = true;
+            button7.Click += NumberButton_Click;
             // 
             // button8
             // 
@@ -115,6 +121,7 @@
             button8.TabIndex = 7;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += NumberButton_Click;
             // 
             // button9
             // 
@@ -124,6 +131,7 @@
             button9.TabIndex = 6;
             button9.Text = "7";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += NumberButton_Click;
             // 
             // button10
             // 
@@ -133,6 +141,7 @@
             button10.TabIndex = 11;
             button10.Text = "=";
             button10.UseVisualStyleBackColor = true;
+            button10.Click += EqualsButton_Click;
             // 
             // button11
             // 
@@ -142,6 +151,7 @@
             button11.TabIndex = 10;
             button11.Text = "0";
             button11.UseVisualStyleBackColor = true;
+            button11.Click += NumberButton_Click;
             // 
             // button12
             // 
@@ -151,6 +161,7 @@
             button12.TabIndex = 9;
             button12.Text = "+";
             button12.UseVisualStyleBackColor = true;
+            button12.Click += OperationButton_Click;
             // 
             // textBox1
             // 
@@ -159,6 +170,7 @@
             textBox1.Size = new Size(174, 23);
             textBox1.TabIndex = 12;
             textBox1.Text = "OUTPUT";
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // Form1
             // 
--- TP_MODUL3_103022430003/Form1.cs
+++ TP_MODUL3_103022430003/Form1.cs
@@ -2,6 +2,10 @@ namespace TP_MODUL3_103022430003
 {
     public partial class Form1 : Form
     {
+        private double result = 0;
+        private string operand = "";
+        private bool isNewNumber = true;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,12 +13,89 @@ namespace TP_MODUL3_103022430003
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            textBox1.Text = "0";
+        }
 
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string number = btn.Text;
+
+            if (isNewNumber)
+            {
+                textBox1.Text = number;
+                isNewNumber = false;
+            }
+            else
+            {
+                textBox1.Text += number;
+            }
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string operation = btn.Text;
+
+            if (!string.IsNullOrEmpty(operand) && !isNewNumber)
+            {
+                // Calculate previous operation
+                CalculateResult();
+            }
+
+            result = double.Parse(textBox1.Text);
+            operand = operation;
+            isNewNumber = true;
+        }
+
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(operand))
+            {
+                CalculateResult();
+                operand = "";
+            }
+        }
+
+        private void CalculateResult()
+        {
+            double currentNumber = double.Parse(textBox1.Text);
+
+            switch (operand)
+            {
+                case "+":
+                    result += currentNumber;
+                    break;
+                case "-":
+                    result -= currentNumber;
+                    break;
+                case "*":
+                    result *= currentNumber;
+                    break;
+                case "/":
+                    if (currentNumber != 0)
+                        result /= currentNumber;
+                    else
+                        textBox1.Text = "Error";
+                    break;
+            }
+
+            textBox1.Text = result.ToString();
+            isNewNumber = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NumberButton_Click(sender, e);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+            NumberButton_Click(sender, e);
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
         }
     }
 }
